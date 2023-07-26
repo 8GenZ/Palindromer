4eae940463930bbc578713f38d2237312f2ebe3f
8Gen <jonachav6414@gmail.com>
2023-07-26 09:41:15 -0400

    added app ui

--- js/site.js
+++ js/site.js
@@ -1,14 +1,55 @@
-function displayMessage() {
-    let inputbox = document.getElementById('message');
-
-    let message = inputbox.value;
+function getValues() {
+    let input = document.getElementById('userString').value;
 
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    )
+    let revString = reverseString(input);
+
+    checkForPalindrome(revString, input)
+
+    displayResults(results)
+}
+
+function reverseString(input) {
+
+    let revString = '';
+
+    for (let index = input.length - 1; index >= 0; index--) {
+
+        let character = input[index];
+
+        revString = revString + character;
+    }
+
+    return revString;
+}
+
+function checkForPalindrome(revString, userString) {
+
+    let results = ''
+
+    if (revString == userString) {
+        results = 'Palindrome';
+    } else {
+        results = 'Not A Palindrome';
+    }
+
+    return results;
+
+}
+
+function displayResults(results) {
+
+    if (results == 'Palindrome') {
+        document.getElementById('true');
+
+        alertBox.classList.remove('invisible');
+
+    }
+    if (results == 'Not A Palindrome') {
+
+        document.getElementById('false')
+
+        alertBox.classList.remove('invisble')
+    }
+
 }
